06463f5ea5e8b0ba30b3d95d96a060ba189c62c0
Antonio Maminiaina Tovomanantsoa <atovoman@c1r4s1.42antananarivo.mg>
2024-06-25 15:24:09 +0300

    Last change

--- src/quiz/quiz.c
+++ src/quiz/quiz.c
@@ -38,7 +38,7 @@ char    **all_questions(void)
     all[6] = strdup("Inona no fanalavana ny V.V.S?\n1-Vy vato Safelika\n2-Vy Vato Sakelika\n3-Vy Vary Sakelika\n");
     all[7] = strdup("Iza no nahita an'i Madagasikara voalohany ?\n1. Marco Polo\n2. Christophe Colomb\n3. Diego Diaz\n");
     all[8] = strdup("Iza no filohan'ny Repoblika voalohany teto Madagasikara ?\n1. Didier RATSIRAKA\n2. Zafy ALBERT\n3. Philbert TSIRANANA\n");
-    all[9] = strdup("Iza no mpanjaka farany teto Madagasikara ?\n1. Rindra II\n2. Rasoherina\n3. Ranavalona III\n");
+    all[9] = strdup("Iza no mpanjaka farany teto Madagasikara ?\n1. Ranavalona IV\n2. Rasoherina\n3. Ranavalona III\n");
     all[10] = strdup("Misy foko firy eto Madagasikara ?\n1. 22\n2. 17\n3. 18\n");
     all[11] = strdup("Inona ny vokatra fanondranana mampalaza an'i Madagasikara?\n1-vanilla\n2-jirofo\n3-vary\n");
     all[12] = strdup("Taiza no natao ny fanambarana ny fahaleovantena an'i madagasikara?\n1-lalan'ny Fahaleovantena\n2-Kianjan'i Mahamasina\n3-Kianjan'ny 13 May\n");
@@ -48,11 +48,35 @@ char    **all_questions(void)
     return (all);
 }
 
+char    **all_rps(void)
+{
+    char **all = malloc(sizeof(char *) * 17);
+    all[0] = strdup("Andry RAJOELINA\n");
+    all[1] = strdup("Antananarivo\n");
+    all[2] = strdup("Ry tanindrazanay malala oh !\n");
+    all[3] = strdup("Fotsy-Mena-Maitso\n");
+    all[4] = strdup("26 Jona 1960\n");
+    all[5] = strdup("vary\n");
+    all[6] = strdup("Vy Vato Sakelika\n");
+    all[7] = strdup("Diego Diaz\n");
+    all[8] = strdup("Philbert TSIRANANA\n");
+    all[9] = strdup("Ranavalona III\n");
+    all[10] = strdup("18\n");
+    all[11] = strdup("vanilla\n");
+    all[12] = strdup("Kianjan'i Mahamasina\n");
+    all[13] = strdup("23\n");
+    all[14] = strdup("Mouvement Democratique pour la Rénovation Malgache\n");
+    all[15] = strdup("toy izay very tsikalakalam-pihavanana.'\n");
+    return (all);
+}
+
+
 bool varavarana(void)
 {
     static int index;
     static int j = 0;
     static int stock[16] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    char **rps = all_rps();
     int i = 2;
     char **all = all_questions();
     static int result[16] = {'3', '2', '2', '2', '2', '2', '2', '3', '3', '3', '3', '1', '2', '1', '1', '1'};
@@ -70,6 +94,7 @@ bool varavarana(void)
             return (true);
         } else if (i == 2){
             printf(RED "Diso ! Manana andrana iray sisa ianao !" RESET "\n");
+            printf("Ny valiny marina dia : %s\n", rps[index]);
         }
         result[index] = '0';
         i--;
